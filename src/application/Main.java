b720b3bab9ac4d3fc2d505dbcc3243db96af55fe
dscalon <dscalon@live.com>
2020-05-29 21:01:27 -0300

    Reusing instantiation (SellerDao.findById)

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,10 +2,7 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.util.Date;
 
 public class Main {
 
@@ -13,6 +10,7 @@ public class Main {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
